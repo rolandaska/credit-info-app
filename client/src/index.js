e372d761471dd4475c2c937c07e3ca0fed4b83ae
Rolandas <kazrolandas@gmail.com>
2020-05-13 13:50:12 +0300

    ui default components and style variables

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 
+import './style/global.scss';
+import './style/ie-fix.scss';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
